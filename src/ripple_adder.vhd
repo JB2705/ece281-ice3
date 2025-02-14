04e6f65c0b814f77ad3515e3351e35de1f2d41e9
JB2705 <c27jonathan.barnett@afacademy.af.edu>
2025-02-13 19:33:20 -0700

    Testcases commit

--- src/ripple_adder.vhd
+++ src/ripple_adder.vhd
@@ -8,7 +8,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ripple_adder is
     Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
            B : in STD_LOGIC_VECTOR (3 downto 0);
-           Cin : in STD_LOGIC;
+           Cin : in STD_LOGIC; --
            S : out STD_LOGIC_VECTOR (3 downto 0);
            Cout : out STD_LOGIC);
 end ripple_adder;
@@ -17,55 +17,56 @@ architecture Behavioral of ripple_adder is
 
     component full_adder is
         port (
-            i_A     : in std_logic;
-            i_B     : in std_logic;
-            i_Cin   : in std_logic;
-            o_S     : out std_logic;
-            o_Cout  : out std_logic
+            A     : in std_logic; --maybe add correct prefixes, i_/o_...
+            B     : in std_logic;
+            Cin   : in std_logic;
+            S     : out std_logic;
+            Cout  : out std_logic
             );
         end component full_adder;
     -- Declare components here
     
     -- Declare signals here
-        signal w_carry  : STD_LOGIC_VECTOR(3 downto 0); -- for ripple between adders
+        signal w_carry  : STD_LOGIC_VECTOR(2 downto 0); -- for ripple between adders, 2 to 0 because only 3 in digital?
 
 begin
 -- PORT MAPS --------------------
     full_adder_0: full_adder
     port map(
-        i_A     => A(0),
-        i_B     => B(0),
-        i_Cin   => Cin,   -- Directly to input here
-        o_S     => S(0),
-        o_Cout  => w_carry(0)
+        A     => A(0),
+        B     => B(0),
+        Cin   => Cin,   -- Directly to input here
+        S     => S(0),
+        Cout  => w_carry(0)
     );
 
     full_adder_1: full_adder
     port map(
-        i_A     => A(1),
-        i_B     => B(1),
-        i_Cin   => w_carry(0),   -- Directly to input here
-        o_S     => S(1),
-        o_Cout  => w_carry(1)
+        A     => A(1),
+        B     => B(1),
+        Cin   => w_carry(0),
+        S     => S(1),
+        Cout  => w_carry(1)
     );
     
     full_adder_2: full_adder
     port map(
-        i_A     => A(2),
-        i_B     => B(2),
-        i_Cin   => w_carry(1),   -- Directly to input here
-        o_S     => S(2),
-        o_Cout  => w_carry(2)
+        A     => A(2),
+        B     => B(2),
+        Cin   => w_carry(1),
+        S     => S(2),
+        Cout  => w_carry(2)
     );
     
     full_adder_3: full_adder
     port map(
-        i_A     => A(3),
-        i_B     => B(3),
-        i_Cin   => w_carry(2),   -- Directly to input here
-        o_S     => S(2),
-        o_Cout  => w_carry(3)
+        A     => A(3),
+        B     => B(3),
+        Cin   => w_carry(2),
+        S     => S(3),
+        Cout  => Cout--goes directly to last Cout
     );
+    
 
 
 end Behavioral;
--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -46,11 +46,18 @@ begin
 	   w_addends <= x"00"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
-       w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+       w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;--addends is an 8 bit number from all the inputs, MSB to LSB, in hex. C in in the single cin bit. Make test cases according to digital. Sum is 4 bit num MSB to LSB of all S in hex. C out is the single Cout
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;--wouldnt it be F for sum?
        -- TODO, a few other test cases
+       --test only Cin input, s(0)=1, Cout=0
        w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"1" and w_Cout = '0') report "bad with only carry" severity failure;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with only cIn" severity failure;
+	   --test in b0 b2, a1 a3, out s0, s1, s2, s3 if A3 is MSB and b0 LSB
+	   w_addends <= x"A5"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "bad with alternating input" severity failure;
+	   --test in B0-3 (10101010)+cin, out sum=0, cout=1
+	   w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with only B input" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
